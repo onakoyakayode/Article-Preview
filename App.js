9e2ca5e7ea7a270e1c5c353c73f4848e20f5c4c1
Onakoya Kayode <onakoyakayode@gmail.com>
2021-11-10 22:04:10 +0100

    Add my Screenshot

---
+++ App.js
@@ -0,0 +1,8 @@
+function shareLink() {
+    var share = document.getElementById("share");
+    if (share.style.display === "none") {
+        share.style.display = "flex";
+    } else {
+        share.style.display = "none"
+    }
+}
